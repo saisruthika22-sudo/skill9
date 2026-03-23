6bd3a4e7c482f1cc43df81a57f3950025bb4967b
saisruthika22-sudo <saisruthika22@gmail.com>
2026-03-23 19:30:38 +0530

    Update student.java

--- src/main/java/com/klu/dto/student.java
+++ src/main/java/com/klu/dto/student.java
@@ -1,5 +1,20 @@
 package com.klu.dto;
-
-public class student {
-
+public class Student {
+    private int id;
+    private String name;
+    private String department;
+ public Student(int id,String name,String department){
+        this.id=id;
+        this.name=name;
+        this.department=department;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getDepartment(){
+        return department;
+    }
 }
